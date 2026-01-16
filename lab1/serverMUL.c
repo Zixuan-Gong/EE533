da5cfea3bcc6c627b9866a95b0abb8f5d07808ab
Zixuan-Gong <zixuang@usc.edu>
2026-01-16 00:53:00 -0500

    Add lab1

---
+++ lab1/serverMUL.c
@@ -0,0 +1,66 @@
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument */
+#include <stdio.h>
+#include <stdlib.h>     
+#include <strings.h>   
+#include <unistd.h>    
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+     int sockfd, newsockfd, portno, clilen, num;
+     char buffer[256];
+     struct sockaddr_in serv_addr, cli_addr;
+     int n;
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+     listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+     if (newsockfd < 0) 
+          error("ERROR on accept");
+
+     num = 0;
+     while (1){
+     bzero(buffer,256);
+     n = read(newsockfd,buffer,255);
+     if (n < 0) error("ERROR reading from socket");
+     if (n==0) {printf("Client disconnected.\n"); break;}
+     num ++;
+     printf("Here is NO.%d message from client: %s\n", num, buffer);
+     if (!strcmp(buffer, "quit\n") || !strcmp(buffer, "exit\n")) break;
+     printf("Server reply (NO.%d): ",num);
+     fflush(stdout);
+     bzero(buffer, 256);
+     if (!fgets(buffer, 255, stdin)) break;
+     n = write(newsockfd, buffer, strlen(buffer));
+     if (n < 0) error("ERROR writing to socket");
+     if (!strcmp(buffer, "quit\n") || !strcmp(buffer, "exit\n")) break;}
+     
+     close(newsockfd);
+     close(sockfd);
+     return 0; 
+}
