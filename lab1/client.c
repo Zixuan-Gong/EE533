6c095de23702c4f7d51a272da641628c39622188
Zixuan-Gong <zixuang@usc.edu>
2026-01-15 21:59:01 -0800

    Update client.c
    Add while (1) for communication all the time and quit

--- lab1/client.c
+++ lab1/client.c
@@ -11,12 +11,12 @@
 void error(char *msg)
 {
     perror(msg);
-    exit(0);
+    exit(1);
 }
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
+    int sockfd, portno, n, num;
 
     struct sockaddr_in serv_addr;
     struct hostent *server;
@@ -43,9 +43,16 @@ int main(int argc, char *argv[])
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
-    printf("Please enter the message: ");
+        
+    printf("Connected. Chat mode: you send first. Type quit to exit.\n ");
+    
+    num=0;
+    while(1){
     bzero(buffer,256);
-    fgets(buffer,255,stdin);
+    num ++;
+    if (!fgets(buffer, 255, stdin)) break;
+    if (!strcmp(buffer, "quit\n") || !strcmp(buffer, "exit\n")) break;
+    printf("Here is NO.%d message: %s", num, buffer);
     n = write(sockfd,buffer,strlen(buffer));
     if (n < 0) 
          error("ERROR writing to socket");
@@ -53,6 +60,9 @@ int main(int argc, char *argv[])
     n = read(sockfd,buffer,255);
     if (n < 0) 
          error("ERROR reading from socket");
-    printf("%s\n",buffer);
+    if(n==0) {printf("Server disconnected.\n"); break;}
+    printf("Here is NO.%d message from server: %s", num, buffer);
+    if (!strcmp(buffer, "quit\n") || !strcmp(buffer, "exit\n")) break;}
+    close(sockfd);
     return 0;
 }
